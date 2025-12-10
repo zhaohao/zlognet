70eee542e1eca9ba9ce96ddad331dadb0993d20c
zhaohao <hao@zhao.im>
2025-12-10 15:51:21 +0800

    something

---
+++ 2024/statistics.js
@@ -0,0 +1,8 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://i.zhao.im/assets/js/analytics.js';
+    script.defer = true;
+    script.setAttribute('data-site-key', 'eea4a266f6ec1bab');
+    script.setAttribute('data-endpoint', 'https://i.zhao.im/api/track.php');
+    document.head.appendChild(script);
+})();
