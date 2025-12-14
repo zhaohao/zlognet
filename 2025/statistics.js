d4b75a0c33539904f55ddd3abd467ad67dc4da6f
zhaohao <hao@zhao.im>
2025-12-15 07:37:45 +0800

    something

--- 2025/statistics.js
+++ 2025/statistics.js
@@ -6,3 +6,12 @@
     script.setAttribute('data-endpoint', 'https://i.zhao.im/api/track.php');
     document.head.appendChild(script);
 })();
+
+(function () {
+  const script = document.createElement('script');
+  script.src = 'https://cloud.umami.is/script.js';
+  script.defer = true;
+  script.dataset.websiteId = '170e4246-7377-4f38-8e3e-fa65207d98c8';
+
+  document.head.appendChild(script);
+})();
