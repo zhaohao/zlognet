f841908f1b5527fb802ca420f3a1b4f2da307c14
zhaohao <hao@zhao.im>
2025-12-27 15:11:09 +0800

    something

--- 2025/statistics.js
+++ 2025/statistics.js
@@ -1,9 +1,9 @@
 (function() {
     var script = document.createElement('script');
-    script.src = 'https://i.zhao.im/assets/js/analytics.js';
+    script.src = 'https://echo.zme.life/assets/js/analytics.js';
     script.defer = true;
     script.setAttribute('data-site-key', 'eea4a266f6ec1bab');
-    script.setAttribute('data-endpoint', 'https://i.zhao.im/api/track.php');
+    script.setAttribute('data-endpoint', 'https://echo.zme.life/api/track.php');
     document.head.appendChild(script);
 })();
 
